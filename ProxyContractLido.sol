809d49e541b03e7d071a912b16d11e4452ae3048
Acaishiba <acaishiba@gmail.com>
2024-12-09 22:00:14 +0800

    add withdraw

--- ProxyContractLido.sol
+++ ProxyContractLido.sol
@@ -8,6 +8,14 @@ interface ILido {
      * @return Amount of stETH minted
      */
     function submit(address referral) external payable returns (uint256);
+
+    /**
+     * @notice Transfer stETH from the contract to a specified address
+     * @param recipient The address of the recipient
+     * @param amount The amount of stETH to transfer (in wei)
+     * @return A boolean indicating success
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
 }
 
 contract EthToStethStaking {
@@ -15,6 +23,7 @@ contract EthToStethStaking {
     mapping(address => uint256) public userStEthBalance; // Mapping to track user's stETH balance
 
     event Staked(address indexed user, uint256 ethAmount, uint256 stEthReceived); // Event to log staking actions
+    event Withdrawn(address indexed user, uint256 stEthAmount); // Event to log withdrawal actions
     event DebugStEth(uint256 stEthReceived); // Debug event to log stETH received for testing purposes
 
     /**
@@ -44,6 +53,25 @@ contract EthToStethStaking {
 
         // Log the staking event
         emit Staked(msg.sender, msg.value, stEthReceived);
+    }
+
+    /**
+     * @notice Withdraw stETH from the contract
+     * @param amount The amount of stETH to withdraw (in wei)
+     */
+    function withdraw(uint256 amount) external {
+        require(amount > 0, "Withdraw amount must be greater than zero");
+        require(userStEthBalance[msg.sender] >= amount, "Insufficient stETH balance");
+
+        // Deduct the amount from the user's balance
+        userStEthBalance[msg.sender] -= amount;
+
+        // Transfer stETH to the user
+        bool success = lido.transfer(msg.sender, amount);
+        require(success, "stETH transfer failed");
+
+        // Log the withdrawal event
+        emit Withdrawn(msg.sender, amount);
     }
 
     /**
